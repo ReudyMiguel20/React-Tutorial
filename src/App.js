a1aeb3395fa74cb0896d38e740c5f0f1578a2a83
Reudy Guerrero <reudymiguel20@gmail.com>
2023-11-07 20:59:22 -0400

    Module 4 part 2

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Planet from './Planet.jsx'
 import { useState } from "react";
 
 function App() {
-const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
-const increaseAge = () => {
-  setAge(age + 1);
-};
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div className="App">
-      {age}
-      <button onClick={increaseAge}>Increase Age</button>
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
     </div>
   );
 }
